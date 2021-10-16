45edaa7f18996608e33e51489cff5f7514bb5e5c
DangNguyen146 <19521317@gm.uit.edu.vn>
2021-10-16 15:52:25 +0700

    fix coutup

--- js/taskService.js
+++ js/taskService.js
@@ -1,7 +1,7 @@
 function taskService() {
   this.getListApi = function () {
     return axios({
-      url: "https://sheet.best/api/sheets/5540ef73-f8fc-448d-8e49-26847887dbd4",
+      url: "https://script.googleusercontent.com/macros/echo?user_content_key=GVg2m3JWfUZ_8nQ1OKg6m5osnAsLgxhibTRGLSpBA9W3DsB161W0-L4R3mrdjV5bQuMOEPg0K1q02iSKbm_X_1mT43xEK6J7m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBQk5qSkN5NYgjTd0_gigP4qIc9hOEwG3Ils6Dhd95p7epL6DaCYGrmq6H-3R11rkyvKCVRyJf8RBOOXz6qNAUb8tGRSuxEpuNz9Jw9Md8uu&lib=MQTObe-KHr7UJqH3hMzG3XSm4-IgYx82c",
       method: "GET",
     });
   };
@@ -10,11 +10,11 @@ var TaskList = new taskService();
 
 TaskList.getListApi()
   .then(function (result) {
-    // console.log(result.data[0]);
+    console.log(result.data);
     console.log("da chay cai nay");
-    document.getElementById("sinhvien").innerHTML = result.data[0].sinhvien;
-    document.getElementById("todoi").innerHTML = result.data[0].todoi;
-    document.getElementById("canhan").innerHTML = result.data[0].canhan;
+    document.getElementById("sinhvien").innerHTML = result.data.sinhvien;
+    document.getElementById("todoi").innerHTML = result.data.todoi;
+    document.getElementById("canhan").innerHTML = result.data.canhan;
   })
   .catch(function (erro) {
     console.log(erro);

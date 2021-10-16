4890bbcd96b5d388d362c71856f393a0431ece7d
DangNguyen146 <19521317@gm.uit.edu.vn>
2021-10-16 22:12:11 +0700

    fix index

--- js/taskService.js
+++ js/taskService.js
@@ -1,7 +1,7 @@
 function taskService() {
   this.getListApi = function () {
     return axios({
-      url: "https://script.googleusercontent.com/macros/echo?user_content_key=GVg2m3JWfUZ_8nQ1OKg6m5osnAsLgxhibTRGLSpBA9W3DsB161W0-L4R3mrdjV5bQuMOEPg0K1q02iSKbm_X_1mT43xEK6J7m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBQk5qSkN5NYgjTd0_gigP4qIc9hOEwG3Ils6Dhd95p7epL6DaCYGrmq6H-3R11rkyvKCVRyJf8RBOOXz6qNAUb8tGRSuxEpuNz9Jw9Md8uu&lib=MQTObe-KHr7UJqH3hMzG3XSm4-IgYx82c",
+      url: "https://script.googleusercontent.com/macros/echo?user_content_key=nt-WvQIXB_zHdsOwr6lucDveIo589DNAiW4_igvYD4levuiMLwSoqHGxx1PXcwWOr7LMfdNq7x_gPo7qXIinsVWJ5KffW8eQm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBQk5qSkN5NYgjTd0_gigP4qIc9hOEwG3Ils6Dhd95p7epL6DaCYGrmq6H-3R11rkyvKCVRyJf8RBOOXz6qNAUb8tGRSuxEpuNz9Jw9Md8uu&lib=MQTObe-KHr7UJqH3hMzG3XSm4-IgYx82c",
       method: "GET",
     });
   };
@@ -17,4 +17,6 @@ TaskList.getListApi()
       window.localStorage.setItem("saveapi", JSON.stringify(result.data));
     }
   })
-  .catch(function (erro) {});
+  .catch(function (erro) {
+    console.log(erro);
+  });

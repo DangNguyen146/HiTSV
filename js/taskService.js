ba89812d1af29670080e37763ac1bb8db8153e12
DangNguyen146 <19521317@gm.uit.edu.vn>
2021-10-16 09:12:38 +0700

    fix soluongdangky

---
+++ js/taskService.js
@@ -0,0 +1,20 @@
+function taskService() {
+  this.getListApi = function () {
+    return axios({
+      url: "https://sheet.best/api/sheets/5540ef73-f8fc-448d-8e49-26847887dbd4",
+      method: "GET",
+    });
+  };
+}
+var TaskList = new taskService();
+
+TaskList.getListApi()
+  .then(function (result) {
+    // console.log(result.data[0]);
+    document.getElementById("sinhvien").innerHTML = result.data[0].sinhvien;
+    document.getElementById("todoi").innerHTML = result.data[0].todoi;
+    document.getElementById("canhan").innerHTML = result.data[0].canhan;
+  })
+  .catch(function (erro) {
+    console.log(erro);
+  });
